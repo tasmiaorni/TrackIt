c6a33aa95a663b29c234edf999a1a9e0d1a39e95
kyrakseeey <62898617+kyrakseeey@users.noreply.github.com>
2020-04-06 19:19:00 -0400

    About has better header with return to home

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public navCtrl: NavController,
+  ) {
+
+  }
 
   ngOnInit() {
+  }
+
+  goToHome() {
+    this.navCtrl.navigateRoot('/home-accounts');
   }
 
 }
